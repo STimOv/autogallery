76f4df3fecab2d182a623dcea81c27a7a7175542
STimOv <tsvamp333@googlemail.com>
2016-11-22 13:38:21 +0100

    Create settings.php

---
+++ lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * english language file for autogallery plugin
+ *
+ * @author Tim Siebentaler <tsvamp333@googlemail.com>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
