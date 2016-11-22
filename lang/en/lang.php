f8a752715ccd64ebed5b8c18c183a96de41279e3
STimOv <tsvamp333@googlemail.com>
2016-11-22 13:37:46 +0100

    Create lang.php

---
+++ lang/en/lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * English language file for autogallery plugin
+ *
+ * @author Tim Siebentaler <tsvamp333@googlemail.com>
+ */
+
+// menu entry for admin plugins
+// $lang['menu'] = 'Your menu entry';
+
+// custom language strings for the plugin
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
