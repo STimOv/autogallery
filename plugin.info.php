83f3068f70fd15d9a69b722a3bf65c8896b3df4d
STimOv <tsvamp333@googlemail.com>
2016-11-22 13:33:09 +0100

    Create plugin.info.php

---
+++ plugin.info.php
@@ -0,0 +1,7 @@
+base   autogallery
+author Tim Siebentaler
+email  tsvamp333@googlemail.com
+date   2016-11-22
+name   autogallery plugin beta1
+desc   Creates a namespace and a Gallery in it, images are stored in a sub-namespace, Needs Plugin Gallery to work! 
+url    https://www.dokuwiki.org/plugin:auto_gallery
